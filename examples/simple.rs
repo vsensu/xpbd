5d18153b3fd799b5000298738842d34a1c9469ed
vsensu <vsensu@foxmail.com>
2026-01-18 17:52:01 +0800

    Representation of objects

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,41 @@
 use bevy::prelude::*;
+use xpbd::*;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup)
         .run();
+}
+
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let sphere = meshes.add(Mesh::from(Sphere { radius: 0.5 }));
+
+    let white = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        unlit: true,
+        ..Default::default()
+    });
+
+    // spawn circle
+    commands.spawn((
+        Mesh3d(sphere),
+        MeshMaterial3d(white.clone()),
+        Pos(Vec2::ZERO),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Projection::from(OrthographicProjection {
+            scale: 0.01,
+            ..OrthographicProjection::default_3d()
+        }),
+        Transform::from_translation(Vec3::new(0., 0., 100.)),
+        Msaa::Sample4,
+    ));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use bevy::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[derive(Component, Debug, Default)]
+pub struct Pos(pub Vec2);
